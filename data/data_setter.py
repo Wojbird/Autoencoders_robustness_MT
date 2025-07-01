7571444e2ce56625ee67cf59d0b125dfcac7dff9
Wojciech Makuch <wojmak0@gmail.com>
2025-07-02 01:54:58 +0200

    ImageNet load fix

--- data/data_setter.py
+++ data/data_setter.py
@@ -4,17 +4,21 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import random_split
 from utils.path_loader import load_imagenet_root
 
+full_root = load_imagenet_root()
+
 
 def get_transforms(image_size):
+
     return transforms.Compose([
         transforms.Resize((image_size, image_size)),
-        transforms.ToTensor()
+        transforms.ToTensor(),
     ])
 
 
 def get_subnet_datasets(root_dir="datasets/subset_imagenet/", image_size=224, val_split=0.1):
+
     transform = get_transforms(image_size)
-    dataset = ImageFolder(root=root_dir, transform=transform)
+    dataset = ImageFolder(root_dir, transform=transform)
 
     val_size = int(len(dataset) * val_split)
     train_size = len(dataset) - val_size
@@ -23,16 +27,11 @@ def get_subnet_datasets(root_dir="datasets/subset_imagenet/", image_size=224, va
     return train_set, val_set
 
 
-def get_imagenet_datasets(root_dir=None, image_size=224):
-    if root_dir is None:
-        root_dir = load_imagenet_root()
+def get_imagenet_datasets(root_dir=full_root, image_size=224):
 
     transform = get_transforms(image_size)
 
-    train_dir = os.path.join(root_dir, "train")
-    val_dir = os.path.join(root_dir, "val")
-
-    train_set = ImageFolder(root=train_dir, transform=transform)
-    val_set = ImageFolder(root=val_dir, transform=transform)
+    train_set = ImageFolder(os.path.join(root_dir, "train"), transform=transform)
+    val_set = ImageFolder(os.path.join(root_dir, "val"), transform=transform)
 
     return train_set, val_set
